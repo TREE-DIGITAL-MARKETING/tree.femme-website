8d2fe024cb0d72e58b407254cd7d53c6629bc017
SheethalJoy <sheethaljoy00001@gmail.com>
2024-12-04 11:58:06 +0530

    about heading align change

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -38,7 +38,7 @@ const About = () => {
   style={{
     backgroundImage:
       'linear-gradient(267.03deg, #00F0FF 4.01%, #5200FF 57.55%, #FF2DF7 114.97%)',
-    whiteSpace: 'nowrap', 
+    
   }}
 >
   Leading digitally, for a better future
