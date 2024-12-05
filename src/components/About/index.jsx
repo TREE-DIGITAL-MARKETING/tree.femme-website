6046b6fb91da0db89b06563598dd28c4f65c1dab
SheethalJoy <sheethaljoy00001@gmail.com>
2024-12-05 10:56:51 +0530

    about change

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -13,44 +13,45 @@ const About = () => {
   className="relative flex flex-col items-center justify-center text-center py-5 sm:py-10 md:py-20 overflow-hidden w-full max-w-[1440px] mb-32"
 >
   <div
-    className="relative z-20 flex flex-col items-center px-4 sm:px-8 bg-cover bg-center bg-blend-overlay w-full h-[35vh] sm:h-[85vh] md:h-[90vh] -mt-8"
+    className="relative z-20 flex flex-col items-center px-4 sm:px-8 bg-cover bg-center bg-blend-overlay w-full h-auto sm:h-[85vh] md:h-[90vh] -mt-8"
     style={{
       backgroundImage: 'url(./image/Aboutbg.png)', 
     }}
   >
     {/* WhatsApp Floating Icon */}
-    <div className="fixed right-0 bottom-12 sm:bottom-6 md:bottom-8 lg:bottom-10 xl:bottom-12 w-[60px] sm:w-[60px] md:w-[70px] lg:w-[80px] xl:w-[90px] h-[60px] sm:h-[60px] md:h-[70px] lg:h-[80px] xl:h-[90px] -mr-5 z-10">
-  <a href="https://wa.me/9074429360">
-    <img
-      src="image/whatsapplogo.png"
-      alt="whatsapplogo"
-      className="w-[30px] h-[30px] sm:w-[35px] sm:h-[35px] md:w-[40px] md:h-[40px] lg:w-[45px] lg:h-[45px] xl:w-[50px] xl:h-[50px] -mt-14"
-    />
-  </a>
-</div>
+    <div className="fixed right-4 bottom-8 sm:bottom-6 md:bottom-8 lg:bottom-10 xl:bottom-12 w-[50px] sm:w-[60px] md:w-[70px] lg:w-[80px] xl:w-[90px] h-[50px] sm:h-[60px] md:h-[70px] lg:h-[80px] xl:h-[90px] z-10">
+      <a href="https://wa.me/9074429360">
+        <img
+          src="image/whatsapplogo.png"
+          alt="whatsapplogo"
+          className="w-[30px] h-[30px] sm:w-[35px] sm:h-[35px] md:w-[40px] md:h-[40px] lg:w-[45px] lg:h-[45px] xl:w-[50px] xl:h-[50px]"
+        />
+      </a>
+    </div>
 
     {/* Heading */}
-    <h1 className="text-[40px] sm:text-[50px] md:text-[60px] font-[500] text-white font-futuraLight mt-16 leading-[50px] sm:leading-[63.1px] ">
-  Hello there!
-  <br />
-  <span
-  className="bg-clip-text text-transparent text-[30px] sm:text-[20px] md:text-[60px] font-futuraLight leading-[30px] sm:leading-[63.1px]"
-  style={{
-    backgroundImage:
-      'linear-gradient(267.03deg, #00F0FF 4.01%, #5200FF 57.55%, #FF2DF7 114.97%)',
-    
-  }}
->
-  Leading digitally, for a better future
-</span>
-</h1>
-    <p className="mt-4 max-w-sm sm:max-w-md md:max-w-lg text-white text-[16px] sm:text-[18px] md:text-[20px] font-[300] leading-relaxed -mb-2">
+    <h1 className="text-[30px] sm:text-[40px] md:text-[50px] font-[500] text-white font-futuraLight mt-16 leading-[40px] sm:leading-[50px] md:leading-[60px]">
+      Hello there!
+      <br />
+      <span
+        className="bg-clip-text text-transparent text-[24px] sm:text-[30px] md:text-[40px] font-futuraLight leading-[28px] sm:leading-[40px]"
+        style={{
+          backgroundImage:
+            'linear-gradient(267.03deg, #00F0FF 4.01%, #5200FF 57.55%, #FF2DF7 114.97%)',
+        }}
+      >
+        Leading digitally, for a better future
+      </span>
+    </h1>
+
+    <p className="mt-4 max-w-xs sm:max-w-md md:max-w-lg text-white text-[14px] sm:text-[16px] md:text-[18px] font-[300] leading-relaxed">
       We are a women-led team of millennials and Gen Z who excel at strategic thinking.
       Give your vision, we will turn it into reality. Let the world notice your brand
       with goosebumps!
     </p>
   </div>
 </section>
+
 
 {/* About Section */}
 <section className="px-4 sm:px-8 md:px-12 lg:pl-36 py-8 sm:py-12 flex justify-center mt-12 sm:mt-12 z-10">
