116b66816d011c5a9156b82263eab0e4b800fcf7
KripaPrince <kripaprince232@gmail.com>
2024-12-10 14:34:55 +0530

    updation of last session

--- src/components/HowItWorks/index.jsx
+++ src/components/HowItWorks/index.jsx
@@ -149,13 +149,14 @@ const HowItWorks = () => {
       </main>
        {/*last section */}
        {/*last section */}
-       <div className="relative text-white h-screen w-full flex items-center justify-center -mt-28 ">
+       <div className="relative text-white h-screen w-full flex items-center justify-center  left-4 lg:-left-12 px-4 lg:px-20 mt-6">
+  {/* Main Content Wrapper */}
   <div className="absolute inset-0 flex justify-center md:justify-center lg:justify-start items-center z-0 max-w-[85%] mx-auto">
     <div className="relative z-10">
       <img
         src=".\image\last 3.gif"
         alt="last session img"
-        className="w-[400px] md:w-[500px] h-[250px] md:h-[300px]"
+        className="w-[300px] md:w-[400px] lg:w-[500px] h-[200px] md:h-[250px] lg:h-[300px]"
         style={{
           border: 'none',
           boxShadow: 'none',
@@ -164,15 +165,12 @@ const HowItWorks = () => {
           WebkitMaskImage: 'radial-gradient(circle, rgba(0, 0, 0, 1), rgba(0, 0, 0, 0) 80%)',
         }}
       />
-     <div
-  className="absolute inset-0 flex flex-col justify-center items-center text-center px-2 lg:px-4"
-  style={{ marginLeft: '-20px' }} // Adjust the value as needed
->
-
-        <h1 className="text-[26px] md:text-[36px] lg:text-[40px] font-[700] leading-tight md:leading-[48px] lg:leading-[52.16px] font-futuraBold">
+      {/* Text Section */}
+      <div className="absolute inset-0 flex flex-col justify-center items-center text-center px-4 lg:px-6">
+        <h1 className="text-[22px] md:text-[36px] lg:text-[40px] font-[700] leading-tight md:leading-[48px] lg:leading-[52.16px] font-futuraBold">
           DISCUSS A PROJECT
         </h1>
-        <p className="font-futuraLightFont text-[16px] md:text-[22px] lg:text-[26px] leading-snug md:leading-[30px] lg:leading-[33.62px]">
+        <p className="font-futuraLightFont text-[14px] md:text-[20px] lg:text-[26px] leading-snug md:leading-[30px] lg:leading-[33.62px] mt-4">
           We know how to mold your project
           <br />
           Let's put the words into action
@@ -181,16 +179,17 @@ const HowItWorks = () => {
     </div>
   </div>
 
-  {/* Content */}
-  <div className="absolute bottom-48 lg:bottom-auto lg:right-40 z-10 flex justify-center lg:justify-end px-2 lg:px-0">
-    <a href="/howitworks">
-      <button className="py-3 px-5 w-[200px] md:w-[250px] lg:w-[290px] h-[45px] md:h-[55px] lg:h-[65px] rounded-xl border-[3px] border-[#622bba] text-white font-futuraBold text-[16px] md:text-[18px] lg:text-[20px] font-[700] leading-[14px]">
-        GET CONSULTATION
-      </button>
-    </a>
-  </div>
+  {/* Button Section */}
+  <div className="absolute bottom-36 md:bottom-24 lg:bottom-auto right-4 lg:right-14 mr-20 z-10 flex justify-end px-4 lg:px-0">
+  <a href="/howitworks">
+    <button className="py-3 px-5 w-[180px] md:w-[220px] lg:w-[290px] h-[40px] md:h-[50px] lg:h-[65px] rounded-xl border-[3px] border-[#622bba] text-white font-futuraBold text-[14px] md:text-[18px] lg:text-[20px] font-[700] leading-[14px] mt-4 md:mt-8">
+      GET CONSULTATION
+    </button>
+  </a>
 </div>
- </div>
+
+</div>
+</div>
     
     
   );
