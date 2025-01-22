3976c484e085e07706be3fb3568a5c5268ade940
SheethalJoy <sheethaljoy00001@gmail.com>
2025-01-22 16:25:44 +0530

    add Google tag (gtag.js)

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -4,6 +4,7 @@ import axios from "axios";
 import { useState } from "react";
 import Timeline from "../ExternalComponents/TimeLine";
 import Success from "../ExternalComponents/Success"; 
+import { Helmet } from "react-helmet";
 
 const Home = () => {
   const formdataFormat = {
@@ -60,9 +61,24 @@ const Home = () => {
     }
   }, []);
 return (<>
-  <meta charSet="UTF-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>tree.femme</title>
+  <Helmet>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>tree.femme</title>
+        {/* Google Tag Manager Script */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-6480P1K4WT"
+        ></script>
+        <script>
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-6480P1K4WT');
+          `}
+        </script>
+      </Helmet>
 
  {/* Hero Section */}
  <section className="px-6 sm:px-10 md:px-16 lg:pl-96 bg-black py-8 sm:py-12 grid grid-cols-1 lg:grid-cols-2 gap-8">
